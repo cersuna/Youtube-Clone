78c26ab574dd0cc0934bb0a6eec5609f9d3eb304
Raphael Co <raphaeljco09@gmail.com>
2023-12-04 15:32:26 -0600

    finished processing unit of webapp

--- video processing service/src/index.ts
+++ video processing service/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import ffmpeg from "fluent-ffmpeg"
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
 app.post("/process-video", (req, res) => {
     // Need to get the path from input video
@@ -11,21 +11,22 @@ app.post("/process-video", (req, res) => {
 
     if (!inputFilePath) {
         res.status(400).send("Bad Request: Missing INPUT path.")
-    } else {
+    } else if (!outputFilePath) {
         res.status(400).send("Bad Request: Missing OUTPUT path")
     }
-
     ffmpeg(inputFilePath)
         .outputOptions("-vf", "scale=1280:720") //make sure this works
         .on("end", () => {
-
+            res.status(200).send(`Video Proceesing Finished Successfully`)
         })
         .on("error", (err) => {
             console.log(`An error occured: ${err.message}`);
-            res.status(500)
+            res.status(500).send(`Internal Server Error: ${err.message}`);
         })
+        .save(outputFilePath);
 })
 
+const port = process.env.PORT || 3000;
 app.listen(port, () =>{
     console.log(`video processing services listening at http://localhost:${port}`)
 })
