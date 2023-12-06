4f9ffc14dc78a8da98847a4e346e27b6c9683591
Raphael Co <raphaeljco09@gmail.com>
2023-12-05 18:13:01 -0600

    completed conversion into cloud based processing -> need to add to firebase

--- video processing service/src/index.ts
+++ video processing service/src/index.ts
@@ -1,27 +1,53 @@
 import express from "express";
-import ffmpeg from "fluent-ffmpeg"
-import { setupDirectories } from "./storage";
+import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories, uploadPorcessedVideo } from "./storage";
 
 setupDirectories();
 
 const app = express();
 app.use(express.json());
 
-app.post("/process-video", (req, res) => {
-    // Need to get the path from input video
-    const inputFilePath = req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
-
-    if (!inputFilePath) {
-        res.status(400).send("Bad Request: Missing INPUT path.")
-    } else if (!outputFilePath) {
-        res.status(400).send("Bad Request: Missing OUTPUT path")
+// invoke this through a cloud pub/sub message queue
+app.post("/process-video", async (req, res) => {
+    let data;
+    try {
+        const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+        data = JSON.parse(message);
+        if (!data.name){
+            throw new Error('Invalid message payload received.')
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send('Bad Request: Missing file name.')
     }
 
-})
+    const inputFileName = data.name;
+    const outputFileName = `.processed-${inputFileName}`;
+
+    // Donwloading raw video from the GCS
+    await downloadRawVideo(inputFileName);
+
+    // Conversion of raw video into 1280x720
+    try {
+        convertVideo
+    }   catch(err){
+            Promise.all([
+                deleteRawVideo(inputFileName),
+                deleteProcessedVideo(inputFileName)
+            ]);
+            return res.status(500).send(`Internval Service error, VIDEO PROCESSING has FAILED`)
+        }
+
+    // Uploading processed video into the GCS
+    await uploadPorcessedVideo(outputFileName);
+
+    await Promise.all([
+        deleteRawVideo(inputFileName),
+        deleteProcessedVideo(inputFileName)
+    ]);
+    return res.status(200).send("PROCESS SUCCESSFUL");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>{
     console.log(`video processing services listening at http://localhost:${port}`)
 })
-
